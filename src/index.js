02a473d530313a0f114fa01ffab21574ef6b539a
asouza <alots.ssa@gmail.com>
2016-11-24 17:39:33 -0200

    adicionando rota para a timeline

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import {Router, Route,browserHistory} from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+		<Router history={browserHistory}>	    	
+	    	<Route path="/timeline" component={App}/>	    	    	
+	    </Router>
+), document.getElementById('root'));
